2ed508a4a9c05daf3289c5cbcb2af7f823e4d2fc
Haixing Hu <starfish@DeepLearningServer.dl-server.starfish.work>
2021-01-31 00:54:40 -0800

    update script

--- benchmark-dl-server.sh
+++ benchmark-dl-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TF_XLA_FLAGS='--tf_xla_auto_jit=2' XLA_FLAGS='--xla_gpu_cuda_data_dir=/usr/local/cuda' ./batch_benchmark.sh 2 2 1 10 2 ./config/config_all
+TF_XLA_FLAGS='--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit --tf_xla_enable_xla_devices' XLA_FLAGS='--xla_gpu_cuda_data_dir=/usr/local/cuda' TF_CPP_MIN_LOG_LEVEL=3 ./batch_benchmark.sh 2 2 1 10 2 ./config/config_all
